e21a332f45b6747e233359639d1d404351714de0
mariolatiffathy <32080032+mariolatiffathy@users.noreply.github.com>
2018-09-17 20:52:17 +0200

    Create content.js

---
+++ AlwaysShoutbox/content.js
@@ -0,0 +1,4 @@
+if (!document.getElementById("nulled-bootstrap")) {
+	// This is not the homepage of Nulled.to ... show the shoutbox
+    document.body.innerHTML = "<center><iframe id=\"Chrome.Extensions.Minecon.AlwaysShoutbox\" height=\"500px\" width=\"780px\" src=\"https://www.nulled.to/index.php\"></iframe><br />Shoutbox shown by AlwaysShoutbox-Nulled.to Chrome extension<br /><strong>Extension</strong> by <a href=\"https://www.nulled.to/!1353011\"><span itemprop=\"name\"><span class=\"aqua\">Minecon</span></span></a></center>" + document.body.innerHTML;
+}
